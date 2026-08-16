90e3d11552ee3966e2940624eaa26179b45c16fd
keegan <keeganpatton@gmail.com>
2026-08-16 14:35:33 -0500

    add wallpapers and update waypaper script in nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -22,10 +22,10 @@ let
   };
 
   # Time Determined Wallpapers
-  morningWall   = "/home/keegan/.config/waypaper/Wallpapers/morning_wallpaper.mp4";
-  afternoonWall = "/home/keegan/.config/waypaper/Wallpapers/day_wallpaper.mp4";
-  eveningWall   = "/home/keegan/.config/waypaper/Wallpapers/evening_wallpaper.mp4";
-  nightWall     = "/home/keegan/.config/waypaper/Wallpapers/night_wallpaper.mp4";
+  morningWall   = "/home/keegan/.config/waypaper/Wallpapers/before-the-road.1920x1080.mp4";
+  afternoonWall = "/home/keegan/.config/waypaper/Wallpapers/room-in-rainy-day.3840x2160.mp4";
+  eveningWall   = "/home/keegan/.config/waypaper/Wallpapers/oblivion-fireplace-pixel.1920x1080.mp4";
+  nightWall     = "/home/keegan/.config/waypaper/Wallpapers/emily-in-the-cyberpunk-city.1920x1080.mp4";
 
 
   dynamicWallpaperScript = pkgs.writeShellScriptBin "dynamic-wallpaper" ''
